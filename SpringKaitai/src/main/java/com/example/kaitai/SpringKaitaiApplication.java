d8b56c4c78cca08d7d022acc6551a5f164cbef1f
yanokama <hikaru32699@gmail.com>
2021-09-18 16:22:01 +0900

    001:初回コミット。プロジェクト作成
    Change-Id: Iba9343d06ef69e3d2e2fc68a3f00306ec6acfe24

---
+++ SpringKaitai/src/main/java/com/example/kaitai/SpringKaitaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kaitai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKaitaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKaitaiApplication.class, args);
+	}
+
+}
