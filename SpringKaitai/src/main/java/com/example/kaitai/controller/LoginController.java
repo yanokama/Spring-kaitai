6d43ea8cf4eca5e49b94254964f7710232706141
yanokama <hikaru32699@gmail.com>
2021-09-20 12:53:52 +0900

    006: 7章レイアウト

--- SpringKaitai/src/main/java/com/example/kaitai/controller/LoginController.java
+++ SpringKaitai/src/main/java/com/example/kaitai/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.kaitai.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -11,4 +12,9 @@ public class LoginController {
 		return "login/login";
 	}
 	
+	/**ユーザー一覧画面にリダイレクト*/
+	@PostMapping("/login")
+	public String postLogin() {
+		return "redirect:/user/list";
+	}
 }
